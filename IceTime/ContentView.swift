448602e9812d41679deaa2a6e430fbccc418ab7b
Lemine <lemine@MacBook-Air-von-Lemine.local>
2024-02-27 01:49:10 +0100

    Initial Commit

---
+++ IceTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IceTime
+//
+//  Created by Lemine Mo El Agheb on 27.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IceTime/IceTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceTimeApp.swift
+//  IceTime
+//
+//  Created by Lemine Mo El Agheb on 27.02.24.
+//
+
+import SwiftUI
+
+@main
+struct IceTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
